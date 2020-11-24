e5b2b3dd8e62ae0c34ca232ea9935b22e37b571b
Hkc-1673 <habibkc71@gmail.com>
2020-11-24 22:09:23 +0200

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from 'react';
 import Signup from "./pages/Signup"
+import Navbar from "./components/Navbar"
 
 function App() {
  
   return (
   
     <>
+    <Navbar/>
      <Signup/>
     </>
     
