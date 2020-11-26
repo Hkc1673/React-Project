4539a3d520964fc877740e79e042df14d666164e
Hkc-1673 <habibkc71@gmail.com>
2020-11-26 20:42:52 +0200

    Add firebase register methods

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -12,6 +12,7 @@ const useStyles = makeStyles({
   });
 
 function Signup() {
+  console.log(firebase)
     const formik = useFormik({
         initialValues: {
             displayName: "",
@@ -19,10 +20,16 @@ function Signup() {
             password: "",
         },
         onSubmit: values => {
-          alert(JSON.stringify(values, null, 2));
+          // alert(JSON.stringify(values, null, 2));
+          firebase.register(values.email, values.password);
         },
       });
     const classes = useStyles();
+
+    const handleGoogleButtonClick = () => {
+      firebase.useGoogleProvider()
+    }
+
     return (
         <Container className={classes.root} maxWidth="sm">
             <form onSubmit={formik.handleSubmit}>
@@ -30,8 +37,8 @@ function Signup() {
                     <Grid item xs={12}><TextField label="Display Name" variant="outlined" onChange={formik.handleChange} value={formik.values.displayName} id="displayName" name="displayName" fullWidth /></Grid>
                     <Grid item xs={12}><TextField label="Email" variant="outlined" fullWidth onChange={formik.handleChange} value={formik.values.email} id="email" name="email" /></Grid>
                     <Grid item xs={12}><TextField label="Password" variant="outlined" fullWidth onChange={formik.handleChange} value={formik.values.password} id="password" name="password" /></Grid>
-                    <Grid item xs={12}><Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button></Grid>
-                    <Grid item xs={12}><Button variant="contained" color="primary" fullWidth>SignUp with Google</Button></Grid>
+                    <Grid item xs={12}><Button type="submit" variant="contained" color="primary" fullWidth>Register</Button></Grid>
+                    <Grid item xs={12}><Button variant="contained" color="primary" fullWidth onClick= {handleGoogleButtonClick}>SignUp with Google</Button></Grid>
                 </Grid>
            </form>
         </Container>
